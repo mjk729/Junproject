714ff8fa6fff0188157b82c51d6825c12a5362b5
mjk729 <kimminjung0729@naver.com>
2023-08-02 19:12:18 +0900

    [김민중] - sts 환경 설정

---
+++ src/main/java/com/jungram/pk/JungramProjectApplication.java
@@ -0,0 +1,16 @@
+package com.jungram.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class JungramProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungramProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jungram/pk/hello/HelloController.java
@@ -0,0 +1,22 @@
+package com.jungram.pk.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World";
+	}
+	
+	
+	@GetMapping("/hello/jsp")
+	public String hellojsp() {
+		return "hello/hello";
+	}
+}
